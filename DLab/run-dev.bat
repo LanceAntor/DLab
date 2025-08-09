1c68aeff14fcf550addffbdf15d378239e96b151
Lance Antor <151009102+LanceAntor@users.noreply.github.com>
2025-08-09 23:28:23 +0800

    Delete some debug bat's

--- DLab/run-dev.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "d:\Github Files\DLab\DLab"
-node "C:\Program Files\nodejs\node_modules\npm\bin\npm-cli.js" run dev
-pause
--- DLab/start-backend.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Starting DLab Backend Server...
-cd backend
-call npm start
-pause
